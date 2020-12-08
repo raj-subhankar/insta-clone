76c2aaa5ae003cec62e8d21f90ac81f2ba761ff5
raj-subhankar <subhankar.rj@gmail.com>
2020-12-08 23:45:19 +0530

    Implement image upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { auth, db } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal'
 import { Button, Input } from '@material-ui/core';
+import ImageUpload from './ImageUpload'
 
 
 function getModalStyle() {
@@ -58,7 +59,7 @@ function App() {
   }, [user, username]);
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
+    db.collection('posts').orderBy("timestamp", "desc").onSnapshot(snapshot => {
       setPosts(snapshot.docs.map(doc => ({
         id: doc.id,
         post: doc.data()
@@ -90,6 +91,7 @@ function App() {
   return (
     <div className="app">
 
+      {user?.displayName ? (<ImageUpload username={user.displayName} />) : (<h3>Sorry, you have to signup to upload</h3>)}
       <Modal 
         open={open}
         onClose={() => setOpen(false)}
